f5b00a89a146b1e3e068aa9e9cc54123066e91a7
Daniel <166452209+star-Daniels@users.noreply.github.com>
2024-11-30 16:31:32 -0300

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,37 @@
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    // Declaração da variável WebView para uso posterior
+    private lateinit var webview: WebView
+
+    // Método chamado quando a atividade é criada
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // Define o layout da atividade
+
+        // Inicializa a WebView buscando pelo ID definido no layout
+        webview = findViewById(R.id.webview)
+        // Define um WebViewClient para a WebView, que permite o carregamento de URLs dentro da WebView
+        webview.webViewClient = WebViewClient()
+        // Habilita o suporte a JavaScript na WebView
+        webview.settings.javaScriptEnabled = true
+
+        // Verifica se há um estado salvo; se não houver, carrega a URL inicial
+        if (savedInstanceState == null) {
+            webview.loadUrl("https://sites.google.com/view/truenewsbrasil/in%C3%ADcio?authuser=0")
+        } else {
+            // Se houver um estado salvo, restaura o estado da WebView
+            webview.restoreState(savedInstanceState)
+        }
+    }
+
+    // Método chamado para salvar o estado da atividade antes de ser destruída
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // Salva o estado atual da WebView no Bundle passado
+        webview.saveState(outState)
+    }
+}
